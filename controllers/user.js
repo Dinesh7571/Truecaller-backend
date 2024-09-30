e7d173950819a4da7358aba156ba384b8ac76593
Dinesh7571 <cat505600@gmail.com>
2024-09-30 20:54:26 +0530

    some error importing user model fixed

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../model/User");
+const User = require("../model/user");
 const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process.env;
 
 // Generate tokens
