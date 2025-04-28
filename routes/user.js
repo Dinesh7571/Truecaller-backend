fb8b85122e7ddf604edf7cf8981442afed6e8ec5
DINESH Kannaujiya <69906526+Dinesh7571@users.noreply.github.com>
2025-04-28 23:10:41 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -14,6 +14,9 @@ router.get("/:phoneNumber", authenticateToken, getUserByPhoneNumber);
 router.put("/report/:phoneNumber", authenticateToken, reportFraud);
 router.post("/add-multiple", authenticateToken, addMultipleUsers);
 router.get("/all-spam/:countryCode",  allSpamUsersByCountryCode);
+router.get('/test',  (req, res) => {
+  res.status(200).json({ message: "Test route is working" });
+});
 router.post("/login", login);
 router.post("/signup", signUp);
 
