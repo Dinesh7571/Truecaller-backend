8076fd7e61f19cb6eb7e25af53101b903806b288
DINESH Kannaujiya <69906526+Dinesh7571@users.noreply.github.com>
2025-04-28 20:59:52 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -6,12 +6,14 @@ const {
   login,
   signUp,
   reportFraud,
+  allSpamUsersByCountryCode,
 } = require("../controllers/user");
 const router = express.Router();
 
 router.get("/:phoneNumber", authenticateToken, getUserByPhoneNumber);
 router.put("/report/:phoneNumber", authenticateToken, reportFraud);
 router.post("/add-multiple", authenticateToken, addMultipleUsers);
+router.get("/all-spam/:countryCode", authenticateToken, allSpamUsersByCountryCode);
 router.post("/login", login);
 router.post("/signup", signUp);
 
